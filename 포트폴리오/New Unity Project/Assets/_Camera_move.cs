75f875d82aae0e8787f1d84aefbf92c0f1c0218c
user-PC\user <vega1423@gmail.com>
2017-02-23 06:01:10 +0900

    작업물 카메라 보는 방향의 y축만 player도 같이 회전  vector3 (0,카메라 y축,0)

--- 포트폴리오/New Unity Project/Assets/_Camera_move.cs
+++ 포트폴리오/New Unity Project/Assets/_Camera_move.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class _Camera_move : MonoBehaviour {
 
     public Camera cam;
+
+
+
+	public float _speed;
+
     public float sensitivityX = 15.0f;
     public float sensitivityY = 15.0f;
 
@@ -16,16 +21,64 @@ public class _Camera_move : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+		//Cursor.lockState = CursorLockMode.Locked;
+		//Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+		
+			//Cursor.lockState = CursorLockMode.Locked;
+			//Cursor.visible = false;
 
-        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+		transform.eulerAngles = new Vector3(0, rotationX,0);
 
-        cam.transform.localEulerAngles = new Vector3(-rotationY, rotationX,0);
+		_camera_view ();
+		Move ();
 	}
+
+
+	void _camera_view()
+	{
+		
+		
+		rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+		rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+		rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+
+		if (rotationX <= 0.0f) {
+			rotationX = 360.0f;
+		}
+		if (rotationX > 360.0f) {
+			rotationX = 0.0f;
+		}
+		cam.transform.eulerAngles = new Vector3(-rotationY, rotationX,0);
+	}
+
+	void Move()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			//float w = Input.GetAxis("Vertical");
+			transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			//float s = Input.GetAxis("Vertical");
+			transform.Translate(Vector3.back * _speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			//float a = Input.GetAxis("Horizontal");
+			transform.Translate(Vector3.left * _speed * Time.deltaTime);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			//float d = Input.GetAxis("Horizontal");
+			transform.Translate(Vector3.right * _speed * Time.deltaTime);
+		}
+	}
+
 }
