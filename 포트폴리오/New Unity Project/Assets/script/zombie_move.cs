30333ee4f69dbbfbcfb0f856dc1657a495075a05
user-PC\user <vega1423@gmail.com>
2017-02-24 00:01:44 +0900

    약간 작업함

---
+++ 포트폴리오/New Unity Project/Assets/script/zombie_move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_move : MonoBehaviour {
+	public float _zomSpeed = 0.0f;
+	public float _speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_speed >= _zomSpeed)
+		{
+			_zomSpeed += 0.1f;
+			transform.Translate (Vector3.forward*_zomSpeed*Time.deltaTime);
+		}
+
+
+		transform.Translate (Vector3.forward*_zomSpeed*Time.deltaTime);
+	}
+}
