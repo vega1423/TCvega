1324ba32a2991c310a455587e77b20c6b06029aa
manavega <tcmana@naver.com>
2017-02-24 14:50:45 +0900

    학원에서 작업
    네비메쉬 완성

--- 포트폴리오/New Unity Project/Assets/script/zombie_move.cs
+++ 포트폴리오/New Unity Project/Assets/script/zombie_move.cs
@@ -1,24 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class zombie_move : MonoBehaviour {
-	public float _zomSpeed = 0.0f;
-	public float _speed;
+
+    public GameObject _player;
+	//public float _zomSpeed = 0.0f;
+	//public float _speed;
+
+    Animation Ani;
+    NavMeshAgent Navi;
 	// Use this for initialization
 	void Start () {
-		
+        Navi = GetComponent<NavMeshAgent>();
+        Ani = GetComponent<Animation>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(_speed >= _zomSpeed)
+        /*if(_speed >= _zomSpeed)
 		{
 			_zomSpeed += 0.1f;
 			transform.Translate (Vector3.forward*_zomSpeed*Time.deltaTime);
 		}
 
 
-		transform.Translate (Vector3.forward*_zomSpeed*Time.deltaTime);
+		transform.Translate (Vector3.forward*_zomSpeed*Time.deltaTime);*/
+
+        _Zonbie();
 	}
+
+    void _Zonbie()
+    {
+        Navi.SetDestination(_player.transform.position);
+
+        if (transform.position == Navi.destination) return;
+    }
 }
